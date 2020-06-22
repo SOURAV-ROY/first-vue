e3775150b0ae889fb310a04e4623c41d90fe50d9
SOURAV ROY <souravroy.info13@gmail.com>
2020-06-22 22:05:07 +0600

    props type required default ✔

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -1,19 +1,33 @@
 <template>
-    <div class="component">
-        <h3>You may view the User Details here</h3>
-        <p>Many Details</p>
-      <p>Your Name : {{name}}</p>
-    </div>
+  <div class="component">
+    <h3>You may view the User Details here</h3>
+    <p>Many Details</p>
+    <!--      <p>Your Name : {{myName}}</p>-->
+    <p>Your Name : {{switchName()}}</p>
+  </div>
 </template>
 
 <script>
   export default {
-    props:['name']
+    // props:['myName'],
+    props: {
+      // myName: String
+      myName: {
+        type: String,
+        required: true,
+        default: "Sourav Roy"
+      }
+    },
+    methods: {
+      switchName() {
+        return this.myName.split("").reverse().join("")
+      }
+    }
   }
 </script>
 
 <style scoped>
-    div {
-        background-color: lightcoral;
-    }
+  div {
+    background-color: lightcoral;
+  }
 </style>
