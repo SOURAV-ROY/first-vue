0b8524a01c7d5cce8d3a01f23f590bebc8ec391c
SOURAV ROY <souravroy.info13@gmail.com>
2020-07-18 14:15:35 +0600

    Vue Slide Transitions Work Awesome

--- src/AnimationsTransitions/Animation.vue
+++ src/AnimationsTransitions/Animation.vue
@@ -2,9 +2,12 @@
   <div class="col-12 text-center">
     <button class="btn btn-info mt-2" @click="show = !show">Show Alert</button>
     <transition name="sou">
-      <div class="alert alert-primary mt-2" role="alert"
-           v-if="show"
-      >
+      <div class="alert alert-primary mt-2" role="alert" v-if="show">
+        A simple primary alert—check it out!
+      </div>
+    </transition>
+    <transition name="slide">
+      <div class="alert alert-primary mt-2" role="alert" v-if="show">
         A simple primary alert—check it out!
       </div>
     </transition>
@@ -39,5 +42,39 @@
   .sou-leave-active {
     transition: opacity 2s;
     opacity: 0;
+  }
+  /*Slide Show By Animations*/
+  .slide-enter {
+    /*transform: translateY(20px);*/
+  }
+
+  .slide-enter-active {
+    animation: slide-in 1s ease-out forwards;
+  }
+
+  .slide-leave {
+
+  }
+
+  .slide-leave-active {
+    animation: slide-out 1s ease-out forwards;
+  }
+
+  @keyframes slide_in {
+    from {
+      transform: translateY(20px);
+    }
+    to {
+      transform: translateY(0);
+    }
+  }
+
+  @keyframes slide-out {
+    from {
+      transform: translateY(0);
+    }
+    to {
+      transform: translateY(20px);
+    }
   }
 </style>
