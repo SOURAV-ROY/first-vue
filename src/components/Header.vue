8de5c8f4be2c8e2c192d8e10865ac4cf9024877a
SOURAV ROY <souravroy.info13@gmail.com>
2020-07-27 13:17:26 +0600

    Navigation from code 💚

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,7 @@
 <template>
-  <ul class="nav justify-content-end">
-    <li class="nav-item">
+  <ul class="nav nav-pills">
       <router-link to="/" class="pr-3">Home</router-link>
-    </li>
-    <li class="nav-item">
       <router-link to="/user" class="pr-3">User</router-link>
-    </li>
-    <li class="nav-item">
-      <router-link to="/person" class="pr-3">Person</router-link>
-    </li>
 
   </ul>
 </template>
--- src/components/newUser/User.vue
+++ src/components/newUser/User.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
-  <h1>User Component</h1>
+    <h1>User Component</h1>
+    <hr>
+    <button class="btn btn-outline-danger" @click="navigateToHome">Go To Home</button>
   </div>
 </template>
 
 <script>
   export default {
-    name: "User"
+    methods: {
+      navigateToHome() {
+        this.$router.push({path: '/'});
+      }
+    }
   }
 </script>
 
