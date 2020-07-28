bf05bdddad83321a144c3e5e9b2677aa0a5ae486
SOURAV ROY <souravroy.info13@gmail.com>
2020-07-29 00:29:15 +0600

    Using the beforeLeave Guard 💻⚙

--- src/components/newUser/UserEdit.vue
+++ src/components/newUser/UserEdit.vue
@@ -1,18 +1,34 @@
 <template>
-<div>
-  <h3>User Edit Component</h3>
-  <p>Local : {{$route.query.local}}</p>
-  <p>Analytics : {{$route.query.q}}</p>
-  <button class="btn btn-outline-success">Confirm</button>
-  <div class="border border-primary" style="height: 700px"></div>
-  <p id="data" class="border border-danger">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, soluta.</p>
-</div>
+  <div>
+    <h3>User Edit Component</h3>
+    <p>Local : {{$route.query.local}}</p>
+    <p>Analytics : {{$route.query.q}}</p>
+    <button class="btn btn-outline-success" @click="confirmed = true">Confirm</button>
+    <div class="border border-primary" style="height: 700px"></div>
+    <p id="data" class="border border-danger">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda,
+      soluta.</p>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "UserEdit"
+  export default {
+    data() {
+      return {
+        confirmed: false
+      }
+    },
+    beforeRouteLeave(to, from, next) {
+      if (this.confirmed) {
+        next();
+      } else {
+        if (confirm('Are You Sure ??')) {
+          next();
+        } else {
+          next(false);
+        }
+      }
     }
+  }
 </script>
 
 <style scoped>
