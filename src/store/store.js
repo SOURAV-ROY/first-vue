589f48a9b2b5f26941e8e70ac6454fd75adc50d7
SOURAV ROY <souravroy.info13@gmail.com>
2020-08-02 00:59:20 +0600

    Using Separate Files(actions, getters, mutations)💙

--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,10 @@ import Vue from "vue";
 import VueX from 'vuex';
 import counter from "./modules/counter";
 
+import * as actions from './actions';
+import * as getters from './getters';
+import * as mutations from './mutations';
+
 Vue.use(VueX);
 
 export const store = new VueX.Store({
@@ -9,50 +13,56 @@ export const store = new VueX.Store({
     counter: 0,
     value: 0
   },
-  getters: {
-    // doubleCounter: state => {
-    //   return state.counter * 10;
-    // },
-    // stringCounter: state => {
-    //   return state.counter + " Clicks";
-    // },
-    value: state => {
-      return state.value;
-    }
-  },
-  mutations: {
-    // increment: (state, payload) => {
-    //   state.counter += payload;
-    // },
-    // decrement: (state, payload) => {
-    //   state.counter -= payload;
-    // },
-    updateValue: (state, payload) => {
-      state.value = payload;
-    }
-  },
-  actions: {
-    // increment: ({commit}, payload) => {
-    //   commit('increment', payload);
-    // },
-    // decrement: ({commit}, payload) => {
-    //   commit('decrement', payload);
-    // },
-    // asyncIncrement: ({commit}, payload) => {
-    //   setTimeout(() => {
-    //     commit('increment', payload.by);
-    //   }, payload.duration)
-    // },
-    // asyncDecrement: ({commit}, payload) => {
-    //   setTimeout(() => {
-    //     commit('decrement', payload.by);
-    //   }, payload.duration)
-    // },
-    updateValue({commit}, payload) {
-      commit('updateValue', payload)
-    }
-  },
+  getters,
+  mutations,
+  actions,
   modules: {
     counter: counter
   }
+  // getters: {
+  // doubleCounter: state => {
+  //   return state.counter * 10;
+  // },
+  // stringCounter: state => {
+  //   return state.counter + " Clicks";
+  // },
+  //   value: state => {
+  //     return state.value;
+  //   }
+  // },
+
+  // mutations: {
+  // increment: (state, payload) => {
+  //   state.counter += payload;
+  // },
+  // decrement: (state, payload) => {
+  //   state.counter -= payload;
+  // },
+  //   updateValue: (state, payload) => {
+  //     state.value = payload;
+  //   }
+  // },
+
+  // actions: {
+  // increment: ({commit}, payload) => {
+  //   commit('increment', payload);
+  // },
+  // decrement: ({commit}, payload) => {
+  //   commit('decrement', payload);
+  // },
+  // asyncIncrement: ({commit}, payload) => {
+  //   setTimeout(() => {
+  //     commit('increment', payload.by);
+  //   }, payload.duration)
+  // },
+  // asyncDecrement: ({commit}, payload) => {
+  //   setTimeout(() => {
+  //     commit('decrement', payload.by);
+  //   }, payload.duration)
+  // },
+  // updateValue({commit}, payload) {
+  //   commit('updateValue', payload)
+  // },
+  // },
+
 });
